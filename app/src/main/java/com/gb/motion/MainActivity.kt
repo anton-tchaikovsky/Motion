0a8a3c0f472d384dff0e9fa0a535df0c4afe4c94
anton-tchaikovsky <106504887+anton-tchaikovsky@users.noreply.github.com>
2023-10-08 08:20:53 +0300

    Initial commit

---
+++ app/src/main/java/com/gb/motion/MainActivity.kt
@@ -0,0 +1,15 @@
+package com.gb.motion
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.gb.motion.databinding.ActivityMainBinding
+
+private lateinit var binding: ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+    }
+}
